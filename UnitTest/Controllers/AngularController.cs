824f9ec7378a85fd9379de3edfd3da604ec7c3c8
cgyqu <cgyqu2639@gmail.com>
2015-09-23 00:27:35 +0800

    新增angularjs demo

---
+++ UnitTest/Controllers/AngularController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitTest.Controllers
+{
+    public class AngularController : Controller
+    {
+        public ActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
